221048100e9fc2db8da459f874571fb34d1134e8
tituHere <99556919+tituHere@users.noreply.github.com>
2023-07-06 23:49:50 +0530

    BASIC

---
+++ 17. Weather Observation Station 14.sql
@@ -0,0 +1,11 @@
+QUESTION:
+https://www.hackerrank.com/challenges/weather-observation-station-14/problem
+
+FUNCTIONS USED:
+ROUND()
+MAX()
+
+QUERY:
+SELECT  ROUND(MAX(LAT_N),4)
+FROM STATION
+WHERE LAT_N <137.2345;
